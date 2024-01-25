e74b1d97b3d0231d34e067be38722257b472cef3
QuaiFederico <quaifede@gmail.com>
2024-01-25 18:05:35 +0100

    commit gigante con un po di robetta carina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ListTournamentsComponent } from './components/list-tournaments/list-tournaments.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
